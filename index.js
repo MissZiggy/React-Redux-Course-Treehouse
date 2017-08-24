a2687791b1a1000fceb8848a8b78decff8579a61
Sandy Davidson <sandy@sandysandy.net>
2017-08-24 22:28:57 +0100

    Building the Stopwatch Logical Component

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Scoreboard from './Scoreboard';
+import Scoreboard from './src/containers/Scoreboard';
 
 render(
   <Scoreboard />,
