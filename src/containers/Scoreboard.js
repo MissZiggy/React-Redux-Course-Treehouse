a1c6e6e7ccf730bb461dd9ba86041d72716c2fd1
Sandy Davidson <sandy@sandysandy.net>
2017-08-24 22:48:42 +0100

    Building the AddPlayerForm Logical Component

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Stopwatch from '../components/Stopwatch';
 import Counter from '../components/Counter';
 import Stats from '../components/Stats';
+import AddPlayerForm from '../components/AddPlayerForm';
 
 const INITIAL_STATE = {
   players: [
@@ -100,42 +101,5 @@ Player.propTypes = {
   onRemove: React.PropTypes.func.isRequired,
   onScoreChange: React.PropTypes.func.isRequired,
 };
-
-const AddPlayerForm = React.createClass({
-  propTypes: {
-    onAdd: React.PropTypes.func.isRequired,
-  },
-
-  getInitialState: function () {
-    return { name: '' };
-  },
-
-  onNameChange: function (e) {
-    const name = e.target.value;
-    this.setState({ name: name });
-  },
-
-  onSubmit: function (e) {
-    if (e) e.preventDefault();
-    this.props.onAdd(this.state.name);
-    this.setState({ name: '' });
-  },
-
-  render: function () {
-    return (
-      <div className="add-player-form">
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.onNameChange}
-            placeholder="Player Name"
-          />
-          <input type="submit" value="Add Player" />
-        </form>
-      </div>
-    );
-  }
-});
 
 export default Scoreboard;
